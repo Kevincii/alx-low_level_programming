cdb656acac2cec9272df50ec0bb43796fe56496a
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-02-16 17:11:37 +0300

    C programming Project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - entry point
- * retun: always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("size of a char: %i bytes(s)\n", sizeof(char));
-	printf("size of an int: %i bytes(s)\n", sizeof(int));
-	printf("size of a long int: %i bytes(s)\n", sizeof(long int));
-	printf("size of a float: %i bytes(s)\n", sizeof(float));
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
