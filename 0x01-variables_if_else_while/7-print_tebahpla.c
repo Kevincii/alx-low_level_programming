9791929db12c09206f3cb92a7471ba0c848387a3
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-02-17 20:07:22 +0300

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint for the function
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'z'; ab >= 'a'; ab--)
+		putchar(ab)
+
+			putchar('\n');
+	return (0);
+}
