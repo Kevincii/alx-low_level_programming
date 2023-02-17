cc797bf39684aa574062dc4480f3da76de16d86f
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-02-17 20:48:38 +0300

    task 8 done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	int num;
 	char ab;
 
-	for (num = 0; num <= 9; num++)
-		putchar(num % 10);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	for (ab = 'a'; ab <= 'f'; ab++)
 		putchar(ab);
