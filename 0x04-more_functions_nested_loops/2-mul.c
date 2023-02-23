32fc412a07e653230482b61c6c81a8e7f111a6d6
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-02-23 19:18:09 +0300

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul -  function that multiplies two integers
+ * @a: a -  integer 1
+ * @b: b - integer 2
+ * Return: mul of a and b
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# low level programming
+## 0x04-more_functions_nested_loops
+it's going well
