20e00d91f3e368ebf58f87b6fc6a4e8f46996f19
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-03-14 12:42:19 +0300

    Prints name of program

--- 0x0A-argc_argv/0-whatsmyname.c1-args.c
+++ 0x0A-argc_argv/0-whatsmyname.c1-args.c
@@ -3,12 +3,11 @@
 
 /**
  * main - function prints the name of the program
- int argc _attribute_((unused)) - function
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 (Success)
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", *argv);
 
