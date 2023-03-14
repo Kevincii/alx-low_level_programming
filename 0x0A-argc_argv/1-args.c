c6dde89d0d57fff44e1c49776f7c9a39f00dc2fc
Kevincii <kevinkiptoo.kk99@gmail.com>
2023-03-14 15:02:40 +0300

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
+	int a, b, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
-		int result = a * b;
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
 
 		printf("%d\n", result);
 		return (0);
